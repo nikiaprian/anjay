dcfea55910f4ad21158f7c2157cd37e6219bc174
Kevin Maulana <kevinmaulanan@gmail.com>
2022-06-24 20:17:29 +0700

    FPC-62 API Like Comment Forum

--- models/blog.go
+++ models/blog.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type Blog struct {
-	ID         uint                 `json:"id"`
-	User       User                 `json:"user"`
-	Photo      string               `json:"photo"`
-	Title      string               `json:"title"`
-	Content    string               `json:"content"`
-	BlogsLikes []BlogsLikesResponse `json:"blog_likes"`
-	TotalLikes int                  `json:"total_likes"`
-	IsYouLike  bool                 `json:"is_you_like"`
-	Tags       []Tag                `json:"tag"`
-	CreatedAt  time.Time            `json:"created_at"`
-	UpdatedAt  time.Time            `json:"updated_at"`
+	ID           uint                 `json:"id"`
+	User         User                 `json:"user"`
+	Photo        string               `json:"photo"`
+	Title        string               `json:"title"`
+	Content      string               `json:"content"`
+	Tags         []Tag                `json:"tag"`
+	BlogsLikes   []BlogsLikesResponse `json:"blog_likes"`
+	TotalLikes   int                  `json:"total_likes"`
+	IsYouLike    bool                 `json:"is_you_like"`
+	TotalComment int                  `json:"total_comment"`
+	CreatedAt    time.Time            `json:"created_at"`
+	UpdatedAt    time.Time            `json:"updated_at"`
 }
 
 type BlogRequest struct {
--- models/commentforum.go
+++ models/commentforum.go
@@ -3,13 +3,16 @@ package models
 import "time"
 
 type CommentForum struct {
-	ID        int       `json:"id"`
-	User      User      `json:"user"`
-	ForumId   *int      `json:"forum_id,omitempty"`
-	Comment   string    `json:"comment"`
-	IsAnswer  bool      `json:"is_answer"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID                int                         `json:"id"`
+	User              User                        `json:"user"`
+	ForumId           *int                        `json:"forum_id,omitempty"`
+	Comment           string                      `json:"comment"`
+	IsAnswer          bool                        `json:"is_answer"`
+	ForumCommentLikes []ForumCommentLikesResponse `json:"forum__comment_likes"`
+	TotalLikes        int                         `json:"total_likes"`
+	IsYouLike         bool                        `json:"is_you_like"`
+	CreatedAt         time.Time                   `json:"created_at"`
+	UpdatedAt         time.Time                   `json:"updated_at"`
 }
 
 type CommentForumRequest struct {
